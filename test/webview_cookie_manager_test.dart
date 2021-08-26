043826cae47ed9f467cab49f6e6201176c7b6b9a
dtyoung <daniel10young@gmail.com>
2021-08-26 15:40:56 +1200

    Ignore invalid cookies when getting cookies
    When calling getCookies(), all cookies for the provided url are serialised.
    There is a possibility that a cookie's value could contain invalid
    characters. If this is the case, a FormatException is thrown during
    the creation of the Cookie object, resulting in no cookies being
    serialised. This is particularly annoying if the desired result of
    calling getCookies() is to fetch information about a particular
    cookie.
    
    This commit handles serialisation of cookies with invalid values
    by catching any cookies with invalid values and removing them from
    the returned list of cookies.

--- test/webview_cookie_manager_test.dart
+++ test/webview_cookie_manager_test.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:webview_cookie_manager/webview_cookie_manager.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('webview_cookie_manager');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  List<Map<String, String>> cookies = [
+    {
+      "name": "valid_cookie",
+      "value": "1111-2222-3333-4444",
+      "path": "/",
+      "domain": "http://www.test.com",
+    },
+    {
+      "name": "invalid_cookie",
+      "value": "1111,2222,3333,4444",
+      "path": "/",
+      "domain": "http://www.test.com",
+    },
+    {
+      "name": "another_valid_cookie",
+      "value": "1111222233334444",
+      "path": "/",
+      "domain": "http://www.test.com",
+    },
+  ];
+
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+      return cookies;
     });
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
+  });
+
+  test('The invalid cookie is ignored', () async {
+    final cookieManager = WebviewCookieManager();
+
+    final cookies = await cookieManager.getCookies('http://www.test.com');
+
+    expect(cookies.length, 2);
+    expect(cookies.first.name, "valid_cookie");
+    expect(cookies.last.name, "another_valid_cookie");
   });
 }
